8f9fcf9d84024096e023f290ad59ab6f1bb74bd3
Hollyw00d <matt@mattjennings.net>
2015-06-09 18:42:17 -0700

    skeleton done

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+// Adding a User with jQuery Assignment
+$(function() {
+
+    $("form").submit(function() {
+
+        // Assign form input values to variables
+        var firstName = $("#first-name").val();
+        var lastName = $("#last-name").val();
+        var emailAddress = $("#email-address").val();
+        var phone = $("#phone").val();
+
+        // Append table to include submitted
+        // input tag values
+        $("table").children("tbody").append("<tr><td>" + firstName + "</td><td>" + lastName + "</td><td>" + emailAddress + "</td><td>" + phone + "</td></tr>");
+        return false;
+    });
+
+    // Reset button
+    $(document).on("click", "button", function() {
+        $("table").find("tbody").remove();
+        $("table").append("<tbody></tbody>");
+    });
+
+});
